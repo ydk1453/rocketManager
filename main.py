b78191beaa263a764904a7d056ad15bafcd14b07
yusuf <yusufdudakli@gmail.com>
2023-09-14 20:22:08 +0300

    bugFix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Created on 12 Eyl 2023
 @author: yusuf
 '''
 
-import requests
+from rest import restObject
 import telemetryReader
 
 from struct import unpack
@@ -14,103 +14,90 @@ import logging
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
 import os
-from flask import Flask, render_template, flash, redirect, url_for,request,jsonify
+from flask import Flask, render_template,request,jsonify
 template_dir = os.path.dirname(__file__)
 template_dir = os.path.join(template_dir, 'templates')
-print(template_dir)
-# Flask constructor takes the name of
-# current module (__name__) as argument.
+
 app = Flask(__name__,template_folder=template_dir)
  
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
+
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
 def index():
  
-    return render_template('asd.html', rockets=rockets)
+    return render_template('index.html', rockets=rockets)
  
 @app.route('/telemetry')
 def telemetry(): 
     rocketId = request.args.get('jsdata')  
-    ret = {}
-    if rocketId in threads.keys(): 
-        ret = threads[rocketId].queue.get()
-        try:
-            
+    telemetryVals = {}
+    telemetryVals['status'] = 'not active'
+    if rocketId in threads.keys():  
+        ret = threads[rocketId].queue.get()  
+        while not threads[rocketId].queue.empty():      
+            ret = threads[rocketId].queue.get()             
+        try:                
             formatedData = unpack(FORMAT, ret)
             jsonData = {}
             jsonData['Altitude'] = formatedData[4]
             jsonData['Speed'] = formatedData[5]
             jsonData['Acceleration'] = formatedData[6]
             jsonData['Thrust'] = formatedData[7]
-            jsonData['Temperature'] = formatedData[8]
-            ret = jsonData
-            
+            jsonData['Temperature'] = formatedData[8]            
+            telemetryVals = jsonData
+            telemetryVals['status'] = 'active'
         except Exception as e:
             pass
         
-    return render_template('telemetry.html', telemetryVals=ret)
+        
+    return render_template('telemetry.html', telemetryVals=telemetryVals)
  
  
  
 @app.route('/rocketDetails')
 def rocketDetails(): 
     rocketId = request.args.get('jsdata')  
-    ret = {}
-    apiUrl = "http://localhost:5000/rockets"
-    response = requests.get(apiUrl, headers={"X-API-Key":"API_KEY_1"})
-    rockets = response.json()
+       
+    apiUrl = 'rockets'
+    _, rockets = restObject.get(apiUrl=apiUrl)
+                  
     details= {}
-    if response.status_code == 200:
-    
-        try:
-            details= {}
-            for rocket in rockets:
-                if 'id' in rocket.keys():
-                    if rocket['id'] == rocketId:
-                        details['status'] = rocket['status']
-                        details['mass'] = rocket['mass']
-                        details['launched-Time'] = rocket['timestamps']['launched']
-                        details['deployed-Time'] = rocket['timestamps']['deployed']
-                        details['failed-Time'] = rocket['timestamps']['failed']
-                        details['cancelled-Time'] = rocket['timestamps']['cancelled']
-                        
-                        details['altitude'] = rocket['altitude']
-                        details['speed'] = rocket['speed']
-                        details['acceleration'] = rocket['acceleration']
-                        details['thrust'] = rocket['thrust']
-                        details['temperature'] = rocket['temperature']
-                        break
-        except Exception as e:
-                pass
+    for rocket in rockets:
+        if 'id' in rocket.keys():
+            if rocket['id'] == rocketId:
+                details['status'] = rocket['status']
+                details['mass'] = rocket['mass']
+                details['launched-Time'] = rocket['timestamps']['launched']
+                details['deployed-Time'] = rocket['timestamps']['deployed']
+                details['failed-Time'] = rocket['timestamps']['failed']
+                details['cancelled-Time'] = rocket['timestamps']['cancelled']
+                
+                details['altitude'] = rocket['altitude']
+                details['speed'] = rocket['speed']
+                details['acceleration'] = rocket['acceleration']
+                details['thrust'] = rocket['thrust']
+                details['temperature'] = rocket['temperature']
+                break
+
             
     return render_template('rocketDetails.html', details=details) 
  
 @app.route('/weather')
 def suggestions():
-    text = request.args.get('jsdata')
-
-    apiUrl = "http://localhost:5000/weather"
-    response = requests.get(apiUrl, headers={"X-API-Key":"API_KEY_1"})
-    weatherResponse = response.json()
-    
-    if response.status_code == 200:
-        weather = weatherResponse
-        if 'precipitation' in weatherResponse.keys():
-            weatherStatus = ""
-            for key,val in weatherResponse['precipitation'].items():
-                if val == True:
-                    weatherStatus += f'-{key}'
+     
+    apiUrl = 'weather'
+    _, weatherResponse = restObject.get(apiUrl=apiUrl)
         
-            weather['weatherStatus'] = weatherStatus
-            weather['precipitation']['probability'] = weatherResponse['precipitation']['probability']*100
-            
-            weather['humidity']= weatherResponse['humidity']*100
+    weather = weatherResponse
+    if 'precipitation' in weatherResponse.keys():
+        weatherStatus = ""
+        for key,val in weatherResponse['precipitation'].items():
+            if val == True:
+                weatherStatus += f'-{key}'
+    
+        weather['weatherStatus'] = weatherStatus
+        weather['precipitation']['probability'] = weatherResponse['precipitation']['probability']*100           
+        weather['humidity']= weatherResponse['humidity']*100
 
     return render_template('suggestions.html', suggestions=weather)
  
@@ -122,19 +109,21 @@ def operations():
     processType = splittedArr[1]
     
     if processType == "launch":
-        apiUrl = f"http://localhost:5000/rocket/{id}/status/launched"
-        response = requests.put(apiUrl, headers={"X-API-Key":"API_KEY_1"})
+        apiUrl = f"rocket/{id}/status/launched"
+        statusCode = restObject.put(apiUrl=apiUrl)        
     elif processType == 'deploy':
-        apiUrl = f"http://localhost:5000/rocket/{id}/status/deployed"
-        response = requests.put(apiUrl, headers={"X-API-Key":"API_KEY_1"})
+        apiUrl = f"rocket/{id}/status/deployed"
+        statusCode = restObject.put(apiUrl=apiUrl)
     elif processType == 'cancel':
-        apiUrl = f"http://localhost:5000/rocket/{id}/status/launched"
-        response = requests.delete(apiUrl, headers={"X-API-Key":"API_KEY_1"})
-    
-    print(response)
-    message = {'text' : 'OP SUC'}
-    
-    return jsonify(message), 200
+        apiUrl = f"rocket/{id}/status/launched"
+        statusCode = restObject.delete(apiUrl=apiUrl)
+       
+    print(statusCode)
+    if statusCode == 200:
+        resp = jsonify({'success': 'Operation is OK'}), statusCode
+    else:
+        resp = jsonify({'error': 'Operation is NOK'}), statusCode
+    return resp
  
 rockets = [] 
 threads = {}
@@ -142,15 +131,12 @@ weather = {}
 # main driver function
 if __name__ == '__main__':
  
-    apiUrl = "http://localhost:5000/rockets"
-    response = requests.get(apiUrl, headers={"X-API-Key":"API_KEY_1"})
-    rockets = response.json()
-    
+    apiUrl = "rockets"   
+    _, rockets = restObject.get(apiUrl=apiUrl)    
+            
     for rocket in rockets:
         readerThread = telemetryReader.telemetryReader(rocket['telemetry']['port'],rocket['telemetry']['host'] )
         readerThread.start()
         threads[rocket['id']] = readerThread
-   
-    # run() method of Flask class runs the application
-    # on the local development server.
+
     app.run(port=12345)
